57e26f2e3791f559e224765977f9f95c1d47a252
vanjazeli <vanjazeli@gmail.com>
2024-09-02 22:28:24 +0200

    Add FadeWrap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import FadeWrap from './components/FadeWrap';
+
 export default function App() {
-	return <div className="container mx-auto">Content</div>;
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-4">
+				<FadeWrap revealElement={<img src="wunder-wasabi.jpg" />}>
+					<button className="underline text-slate-200 transition-all hover:opacity-60 hover:text-purple-600" type="button">
+						Hover me
+					</button>
+				</FadeWrap>
+			</div>
+		</div>
+	);
 }
---
+++ src/components/FadeWrap.tsx
@@ -0,0 +1,66 @@
+import useMousePosition from '../hooks/useMousePosition';
+import { ReactElement, ReactNode, useState, useRef, useEffect } from 'react';
+import React from 'react';
+import gsap from 'gsap';
+
+type FadeWrapProps = {
+	className?: string;
+	revealElement: ReactElement;
+	children: ReactNode;
+};
+
+export default function FadeWrap({ className, revealElement, children }: FadeWrapProps) {
+	const [isHovered, setIsHovered] = useState(false);
+	const [isAnimating, setIsAnimating] = useState(false);
+
+	const animationWrap = useRef<HTMLDivElement>(null);
+	const animationDuration = 0.1;
+	const timeline = useRef<gsap.core.Timeline | null>(null);
+
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
+	useEffect(() => {
+		if (isHovered) {
+			gsap.killTweensOf(animationWrap.current);
+			timeline.current = gsap
+				.timeline({ onStart: () => setIsAnimating(true) })
+				.add('startLabel')
+				.add(gsap.to(animationWrap.current, { opacity: 1, duration: animationDuration }), 'startLabel');
+		}
+		if (!isHovered && isAnimating) {
+			gsap.killTweensOf(animationWrap.current);
+			timeline.current = gsap
+				.timeline({ onComplete: () => setIsAnimating(false) })
+				.add('startLabel')
+				.add(gsap.to(animationWrap.current, { opacity: 0, duration: animationDuration }), 'startLabel');
+		}
+	}, [isAnimating, isHovered]);
+
+	const { x, y } = useMousePosition();
+
+	const styledRevealElement = React.cloneElement(revealElement, {
+		style: {
+			...revealElement.props.style,
+			objectFit: 'cover',
+			width: '100%',
+			height: '100%',
+		},
+	});
+
+	return (
+		<span className={className} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+			{children}
+			{(isHovered || isAnimating) && (
+				<div style={{ position: 'fixed', top: `${y + 20}px`, left: `${x + 20}px`, height: '160px', width: '240px', pointerEvents: 'none', opacity: '0', overflow: 'hidden' }} ref={animationWrap}>
+					{styledRevealElement}
+				</div>
+			)}
+		</span>
+	);
+}
